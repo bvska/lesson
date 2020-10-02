7c49b0e0fe9655349668bf1cb6c218caae3fb4d0
bvska <pantera24816@gmail.com>
2020-10-02 18:41:31 +0300

    hello2

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         int[][] ints5 = {
                 {2, 3, 45},
-                {6, 7, 81, 8, 4},
+                {6, 7, 81, 8, 4, 7},
                 {16, -7, 801, 228}
         };
         for (int i = 0; i < ints5.length; i++) {
